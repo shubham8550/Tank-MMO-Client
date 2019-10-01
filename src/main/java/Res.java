30fb6166ef72185726a2d557b2c1fc9a6e50c9fe
Shubham Badgujar <shubhambadgujar222@gmail.com>
2019-10-01 14:58:05 +0530

    socket added but server ist fully compatible

--- src/main/java/Res.java
+++ src/main/java/Res.java
@@ -5,6 +5,10 @@ public class Res {
     public static int windowWidth=600;
     public static int vehicalImgWidth=30;
     public static int vehicalImgHeight=30;
+    public static int serverPort=7777;
+    public static String serverAddress="localhost";
+    public static int clientID=0;
+
 
 
 }
---
+++ src/main/java/WorldStat.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+class WorldStat implements Serializable
+{
+    ArrayList<Integer> id=new ArrayList<Integer>();
+    ArrayList<String> name=new ArrayList<String>();
+
+    ArrayList<String> carModle=new ArrayList<String>();
+
+    ArrayList<Integer> X=new ArrayList<Integer>();
+    ArrayList<Integer> Y=new ArrayList<Integer>();
+    ArrayList<Character> face=new ArrayList<Character>();
+
+
+
+
+}
